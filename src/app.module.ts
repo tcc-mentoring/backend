df90e44543572c962146274cafbce97fd5fad2f6
felipe ricardi <felipericardi5@gmail.com>
2023-01-22 00:24:18 -0300

    feat: initial project

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'mentoring',
+      synchronize: true,
+      entities: [
+        User
+      ]
+    }),
+    UsersModule
+  ],
+})
+export class AppModule {}
