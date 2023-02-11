7a3d979f91f6fa0998ce3773ad59200a882fb9b6
felipe ricardi <felipericardi5@gmail.com>
2023-02-11 16:27:52 -0300

    feat: adding endpoint do authenticate user from uuid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post } from "@nestjs/common";
-import { AuthDetails, CreateUserDTO, LoginDTO, User } from "./user.entity";
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from "@nestjs/common";
+import { AuthDetails, CreateUserDTO, LoginDTO, User, UserDTO } from "./user.entity";
 import { UsersService } from "./users.service";
 
 @Controller('user')
@@ -15,6 +15,12 @@ export class UsersController {
     async login(@Body() loginDTO: LoginDTO): Promise<AuthDetails> {
         const auth = await this.usersService.login(loginDTO);
         return auth;
+    }
+
+    @Get('/auth/:userAuthUUID')
+    async auth(@Param() params): Promise<UserDTO> {
+        const user = await this.usersService.findUserByUUID(params.userAuthUUID);
+        return user;
     }
 
     @Get()
