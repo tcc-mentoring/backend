766262982cc404348cdd8ac29f0fce49cc4aa996
felipe ricardi <felipericardi5@gmail.com>
2023-02-18 12:54:51 -0300

    feat: adding auth module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { UsersModule } from './users/users.module';
         User
       ]
     }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
 })
 export class AppModule {}
