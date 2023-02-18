03998785464bb96109375e0ff209fd2e8e4035b4
felipe ricardi <felipericardi5@gmail.com>
2023-02-18 16:05:35 -0300

    feat: adding config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'mentoring',
