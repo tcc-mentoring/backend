4f359bf34e2c2053a070e852a3c5d7fabf4aa5a4
felipe ricardi <felipericardi5@gmail.com>
2023-02-18 19:52:31 -0300

    feat: importing profile module into user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProfileModule } from "src/profile/profile.module";
 import { User } from "./user.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User]), ProfileModule],
     providers: [UsersService],
     controllers: [UsersController],
     exports: [UsersService]
