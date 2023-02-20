bf007616315350f1c2d784fbdfe4ca0e539b2264
felipe ricardi <felipericardi5@gmail.com>
2023-02-20 18:10:44 -0300

    feat: improving profile facade

--- src/facade/ProfileFacade.ts
+++ src/facade/ProfileFacade.ts
@@ -1,9 +1,10 @@
-import { AcademyEntryDTO, OcupationDTO, Profile, ProfileDTO } from "src/profile/profile.entity";
+import { AcademyEntryDTO, CreateOcupationDTO, Ocupation, OcupationDTO, Profile, ProfileDTO } from "src/profile/profile.entity";
 
 export function createProfileDTOFromEntity(profileEntity: Profile): ProfileDTO {
     const academyEntries = 
         profileEntity.academyEntries.map((academyEntry): AcademyEntryDTO => {
             return {
+                    id: academyEntry.id,
                     course: academyEntry.course,
                     startDate: academyEntry.startDate,
                     endDate: academyEntry.endDate,
@@ -14,7 +15,9 @@ export function createProfileDTOFromEntity(profileEntity: Profile): ProfileDTO {
     const ocupations = 
         profileEntity.ocupations.map((ocupation): OcupationDTO => {
             return {
-                description: ocupation.description,
+                    id: ocupation.id,
+                    company: ocupation.company,
+                    description: ocupation.description,
                     startDate: ocupation.startDate,
                     endDate: ocupation.endDate,
             }
@@ -24,4 +27,15 @@ export function createProfileDTOFromEntity(profileEntity: Profile): ProfileDTO {
         academyEntries,
         ocupations
     }
+}
+
+export function createOcupationEntityFromDTO(ocupationDTO: CreateOcupationDTO): Ocupation {
+    const ocupationEntity = new Ocupation();
+
+    ocupationEntity.description = ocupationDTO.description;
+    ocupationEntity.company = ocupationDTO.company;
+    ocupationEntity.startDate = ocupationDTO.startDate;
+    ocupationEntity.endDate = ocupationDTO.endDate; 
+
+    return ocupationEntity;
 }
